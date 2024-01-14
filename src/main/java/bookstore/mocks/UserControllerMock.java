7b2678ac7c958f2e08d35c30c9126c269471b368
Enes-Yusuf-Brucaj <enesbrucaj@gmail.com>
2024-01-14 17:54:28 +0100

    Revert "Merge branch 'master' of https://github.com/RalfPoni/BookStore"
    This reverts commit c6422d36f6eb524594b2828ba20f5600c249337d, reversing
    changes made to 45cbe051000b9e7ebdc37a62a2f78616e9f5cbd8.

---
+++ src/main/java/bookstore/mocks/UserControllerMock.java
@@ -0,0 +1,75 @@
+package bookstore.mocks;
+
+import bookstore.controllers.UserController;
+import bookstore.models.User;
+
+import java.util.ArrayList;
+
+public class UserControllerMock extends UserController {
+
+    private final ArrayList<User> mockedUsers;
+
+    public UserControllerMock() {
+        mockedUsers = new ArrayList<>();
+    }
+
+    @Override
+    public void addUser(User user) {
+        mockedUsers.add(user);
+    }
+
+    @Override
+    public ArrayList<User> getUsers() {
+        return mockedUsers;
+    }
+
+    @Override
+    public void writeUser(User user) {
+        mockedUsers.add(user);
+    }
+
+    @Override
+    public void deleteUser(String name, String lastName) {
+        mockedUsers.removeIf(user -> name.equals(user.getFirstName()) && lastName.equals(user.getLastName()));
+    }
+
+    @Override
+    public void editUser(User userCompare, User user) {
+        int index = mockedUsers.indexOf(userCompare);
+        if (index != -1) {
+            mockedUsers.set(index, user);
+        }
+    }
+
+    @Override
+    public int getUserIndex(String name, String lastName) {
+        for (int i = 0; i < mockedUsers.size(); i++) {
+            if (name.equals(mockedUsers.get(i).getFirstName()) && lastName.equals(mockedUsers.get(i).getLastName())) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    @Override
+    public User getUser(String name, String lastName) {
+        for (User user : mockedUsers) {
+            if (name.equals(user.getFirstName()) && lastName.equals(user.getLastName())) {
+                return user;
+            }
+        }
+        return null;
+    }
+
+    @Override
+    public boolean verifyUser(String email, String password) {
+        return mockedUsers.stream()
+                .anyMatch(user -> email.equals(user.getEmail()) && password.equals(user.getPassword()));
+    }
+
+    @Override
+    public void listToFile()
+    {
+    }
+
+}
